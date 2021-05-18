7b43ccc98824f01939c728cced2f71482ee2baa0
Mahati29 <84368590+Mahati29@users.noreply.github.com>
2021-05-18 11:13:16 +0530

    Add files via upload

---
+++ fib_search.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int fib_search(int n)
+{
+	int a=0,b=1,c,i;
+	if(n==0||n==1)
+	{
+		return 1;
+	}
+	while(n>0)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		if(n==c)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int n;
+	scanf("%d ",&n);
+	fib_search(n);
+	if(fib_search(n))
+    {
+    	printf("true");
+    }
+    else
+    {
+    	printf("false");
+	}
+}
